bdc52f88b7526808ff456a291c673641e2c27d69
MoHeng <ylm200322400@163.com>
2021-07-08 18:07:49 +0800

    更新

---
+++ src/mw/BaseBoot.hx
@@ -0,0 +1,13 @@
+package mw;
+
+import haxe.macro.Type;
+
+/**
+ * 基础游戏引导类
+ * 单例模式
+ */
+class BaseBoot extends hxd.App {
+    static var ME:BaseBoot = new BaseBoot();
+
+    
+}
---
+++ src/mw/event/MainEventLoop.hx
@@ -0,0 +1,7 @@
+package mw.event;
+
+import sys.thread.EventLoop;
+
+class MainEventLoop extends EventLoop{
+    
+}
